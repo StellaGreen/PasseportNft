4d4bc2d47bc7fd11d105d1456937ad2274beb85e
StellaGreen <vickysoler5@gmail.com>
2021-10-30 15:27:12 +0200

    erc721

--- contracts/passeport.sol
+++ contracts/passeport.sol
@@ -1,7 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 contract Passeport {
 
-    
+    // constructor
+
+    // enum ?
+
+    // struct
+
+    // function
 }
