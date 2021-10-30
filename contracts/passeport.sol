9a84c2d20c7cb509d9c8689e50c59d60a24fb867
StellaGreen <vickysoler5@gmail.com>
2021-10-30 15:37:39 +0200

    beggening

--- contracts/passeport.sol
+++ contracts/passeport.sol
@@ -3,9 +3,22 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Passeport {
+contract Passeport is ERC721 {
+
+        // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // mapping of people is register
+    mapping(uint256 => address) private _register;
 
     // constructor
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_){
+        _name =  name_;
+        _symbol = symbol_;
+    }
 
     // enum ?
 
