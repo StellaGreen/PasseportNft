335da79498554f5e81cf34a8a96ad6e6adc728ac
Benmissi-A <aminebenmissi@hotmail.fr>
2021-11-06 16:19:36 +0100

    add events Registering

--- contracts/passeportMapping.sol
+++ contracts/passeportMapping.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Passeport is ERC721 {
-
-// worker
+    // worker
 
     mapping(address => string) private _pseudo;
     mapping(address => uint256) private _age;
@@ -13,43 +12,48 @@ contract Passeport is ERC721 {
     mapping(address => string) private _lang;
     mapping(address => string) private _skills;
 
-
     // mapping of people is register
-    
+
     mapping(address => bool) private _worker;
 
     mapping(address => bool) private _pro;
-    
+
     // mapping of the register balance
     mapping(address => uint256) private _balances;
 
+    event RegisterdWorker(address indexed)
+    event RegisterdPro(address indexed)
+
     // constructor
-    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_){
-        _name =  name_;
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+        _name = name_;
         _symbol = symbol_;
     }
 
     // function
 
-    function registerWorker (string memory countri, uint256 age, ) public {
-        require (msg.sender != _register, "registerWorker : you are register");
-        msg.sender += _register;
+    function registerWorker() public {
+        require(_worker[msg.sender] == false, "PasseportNFT: you are already registered as worker");
+        require(_pro[msg.sender] == false, "PasseportNFT: you are already registered as pro");
+        _worker[msg.sender] = true;
 
-
+        emit RegisterdWorker(msg.sender)
     }
 
-    function registerPro () public {
-        require (msg.sender != _register, "registerWorker : you are register");
-        msg.sender += _register;
+    function registerPro() public {
+        require(_pro[msg.sender] == false, "PasseportNFT: you are already registered as pro");
+        require(_worker[msg.sender] == false, "PasseportNFT: you are already registered as worker");
+        _worker[msg.sender] = true;
+
+        emit RegisterdPro(msg.sender)
 
     }
-
 
     //function view
 
-    function balanceOf (address user) public view returns (uint256){
+    function balanceOf(address user) public view returns (uint256) {
         require(user == _register, "balanceOf :you are not registred");
         require(owner != address(0), "balanceOf : balance query for the zero address");
-    return _balance[_register];
+        return _balance[_register];
     }
 }
